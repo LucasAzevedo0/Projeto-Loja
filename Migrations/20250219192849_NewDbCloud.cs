ab66b8f4d7dc914570b0dafb6fcbf6c64b258b39
Lucas <lucasvictorazevedo@gmail.com>
2025-02-19 15:53:49 -0400

    Azure Database Test

---
+++ Migrations/20250219192849_NewDbCloud.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LojaRemastered.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewDbCloud : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
