fdb4751a7dc49300d6c39ea074be04942a21dc78
Lucas <lucasvictorazevedo@gmail.com>
2025-02-20 18:11:51 -0400

    Final FRONT-END Adds

--- Controllers/TransactionsController.cs
+++ Controllers/TransactionsController.cs
@@ -438,7 +438,7 @@ namespace LojaRemastered.Controllers
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
             var user = await _userManager.FindByIdAsync(userId);
             // Passa o saldo atual para a view
-            ViewBag.CurrentBalance = user.Balance;
+                ViewBag.CurrentBalance = user.Balance;
 
             var transactions = await _context.Transactions
                 .Where(t => t.UserId == userId)
